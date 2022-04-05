5756bf17787e68ed56f6e20e24119f7f35be87f6
Victor Mateus <victordiniz_@outlook.com>
2022-04-05 11:58:38 -0400

    Parte de cima da ampulheta estática OK!

--- ampulheta.js
+++ ampulheta.js
@@ -1,19 +1,39 @@
-const tam = 10;
+const tam = 20;
+let filledLine = "";
 let topLine = "#";
 let bottomLine = "#";
+let empytLine = "#";
 
-for (let i = 0; i <= tam; i++){
-    topLine += "#";
+for (let i = 0; i < tam; i++){
+    filledLine += "#";
+    empytLine += " ";
 }
 
-topLine += "#";
+topLine += filledLine + "#";
+empytLine += "#";
 
-// for (let i = 0; i < tam + 1; i++){
-//     console.log(topLine);
-//     let topLine = topLine.substring(3, topLine.length - 3)
+function replaceCharacter(str, index, index2, filledLine) {
+    let newStr;
+    newStr = str.substr(0, index) + filledLine + str.substr(str.length - index, str.length);
+    // newStr = newStr.substr(0, index2)
+    return newStr;
+}
 
-// }
+function lineBuilder(currentlyLine, filledLine, tam){
+    let j = currentlyLine.length - 1;
+    let z = 0;
+    for(let i = 1; i <= tam; i++){
+        
+        let sand = filledLine.substr(0, filledLine.length - z)
+        
+        let newLine = replaceCharacter(currentlyLine, i, j-i, sand);
+        console.log(newLine);
+
+        z = i * 2;
+    }
+    // console.clear();
+}
 
 console.log(topLine);
-topLine = topLine.substring(3, topLine.length - 3)
+lineBuilder(empytLine, filledLine, tam);
 console.log(topLine);
